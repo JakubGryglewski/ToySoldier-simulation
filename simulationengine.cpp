5551e959f2a0f1fbb85d828c3e2c9726c47bfcc2
Jakub Gryglewski <kpgrygle@gmail.com>
2026-05-17 11:04:34 +0200

    wersja 1

---
+++ simulationengine.cpp
@@ -0,0 +1,3 @@
+#include "simulationengine.h"
+
+simulationengine::simulationengine() {}
---
+++ simulationengine.h
@@ -0,0 +1,14 @@
+#ifndef SIMULATIONENGINE_H
+#define SIMULATIONENGINE_H
+
+#include "squad.h"
+
+enum class SimulationStatus { STOPPED, RUNNING, PAUSED };
+
+class simulationengine
+{
+public:
+    simulationengine();
+};
+
+#endif // SIMULATIONENGINE_H
